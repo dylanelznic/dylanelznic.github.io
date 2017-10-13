971c3da1b61ef30debf3f6519795eeb1e4f6eb44
dylanelznic <djey96@mst.edu>
2017-10-13 02:13:49 -0500

    [UPDATE] Fixed agent position. Will scroll no lower than the top of the messaage box.

--- js/agent-scroll.js
+++ js/agent-scroll.js
@@ -1,6 +1,7 @@
 $(window).scroll(function() {
       var contactHeight = $(".contact-container").height()
-      var base = contactHeight * 0.5
+      var mapHeight = $(".map-container").height()
+      var base = contactHeight + mapHeight - ($(window).height() * 0.63)
 
-	  $(".agent-img").css("bottom", Math.max(0, base - ($(document).height() - $(window).scrollTop() - $(window).height())));
+      $(".agent-img").css("bottom", Math.max(0, base - ($(document).height() - $(window).scrollTop() - $(window).height())));
 });
